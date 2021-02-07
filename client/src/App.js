213d747bd2209eea839c99a4dc26127cc6430b69
eugene@git config --global user.name eugenegit config --global user.name eugene@ <eugnazaroff@gmail.com>
2021-02-07 15:47:51 +0400

    created base react app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <section>
+        <header>
+          <h1>Hello from app</h1>
+          <p>This is base react-app</p>
+        </header>
+      </section>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    port: 4000,
+    hot: true,
+  },
+};
